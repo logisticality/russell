39bcc4bc4879892eb92d8c11812c0667e3294444
Henry Zheng <henry.zheng727@gmail.com>
2026-04-23 00:21:11 -0600

    update Env and Value types to be more functional

--- src/interpreter/treewalk/mod.rs
+++ src/interpreter/treewalk/mod.rs
@@ -1,20 +1,30 @@
-use std::process::ExitCode;
+use std::rc::Rc;
 
-use crate::frontend::parser::ast::Defn;
-use crate::interpreter::treewalk::types::Env;
+use crate::frontend::parser::ast::{Binding, Defn};
+use crate::interpreter::treewalk::types::{Env, Fn};
 
+mod interp_expr;
+mod interp_fn;
 mod types;
 
-pub fn interp(defns: Vec<Defn>) -> ExitCode {
-    for defn in defns {
-        match defn {
-            Defn::Typedef(_, items) => todo!(),
-            Defn::Fn(_, bindings, _, stmts) => todo!(),
-        }
-    }
+pub fn interp(defns: Vec<Defn>) {
+    let (global_env, fn_defs) = process_global_env(defns);
     unimplemented!()
 }
 
-fn process_global_env(defns: Vec<Defn>) -> (Env, Vec<Defn>) {
+fn process_global_env(defns: Vec<Defn>) -> (Rc<Env>, Vec<Fn>) {
+    let mut fn_defs = Vec::new();
+    let mut env = Env::new().into();
+    for defn in defns {
+        match defn {
+            Defn::Typedef(_, items) => env = add_typedef(env, items),
+            Defn::Fn(id, bindings, _, stmts) => fn_defs.push(Fn::new(id, bindings, stmts)),
+        }
+    }
+
+    (env, fn_defs)
+}
+
+fn add_typedef(env: Rc<Env>, bindings: Vec<(String, Vec<Binding>)>) -> Rc<Env> {
     unimplemented!()
 }
--- src/interpreter/treewalk/types.rs
+++ src/interpreter/treewalk/types.rs
@@ -1,30 +1,53 @@
 use std::{collections::HashMap, rc::Rc};
 
+use crate::frontend::parser::ast::{Binding, Expr, Stmt};
+
 pub(super) struct Env {
-    parent: Option<Rc<Env>>,
-    symbols: HashMap<String, Value>,
+    pub next: Option<Rc<Env>>,
+    pub binding: (String, Rc<Value>),
+}
+
+pub(super) struct Fn {
+    pub name: String,
+    pub bindings: Vec<Binding>,
+    pub statements: Vec<Stmt>,
+}
+
+impl Fn {
+    pub(super) fn new(name: String, bindings: Vec<Binding>, statements: Vec<Stmt>) -> Self {
+        Self {
+            name,
+            bindings,
+            statements,
+        }
+    }
 }
 
 impl Env {
-    fn lookup(&self, key: &str) -> Option<&Value> {
-        if self.symbols.contains_key(key) {
-            self.symbols.get(key)
-        } else {
-            match &self.parent {
-                Some(p) => p.lookup(key),
-                None => None,
-            }
-        }
+    pub(super) fn new() -> Env {
+        unimplemented!()
     }
 
-    fn define(&mut self, key: String, value: Value) {
-        self.symbols.insert(key, value);
+    pub(super) fn extend(&self, id: String, val: Rc<Value>) -> Rc<Env> {
+        unimplemented!()
+    }
+
+    pub(super) fn lookup(&self, key: &str) -> Option<Rc<Value>> {
+        if self.binding.0 == key {
+            return Some(Rc::clone(&self.binding.1));
+        }
+
+        match &self.next {
+            Some(env) => env.lookup(key),
+            None => None,
+        }
     }
 }
 
 pub(super) enum Value {
-    Int,
-    Float,
-    Bool,
-    Closure,
+    Int(i64),
+    Float(f64),
+    Bool(bool),
+    Closure(Rc<Env>, Binding, Box<Expr>),
+    Adt(String, HashMap<String, Rc<Value>>),
 }
